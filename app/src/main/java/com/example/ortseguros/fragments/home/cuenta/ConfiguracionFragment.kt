2076f95e2116364a89183bd3ff6cf5ee40fe50ac
Lucas Alonso <lucasalonso42@gmail.com>
2023-10-28 11:29:33 -0300

    Hacer llamada desde botones de Cuenta y agrego permiso en el Manifest

--- app/src/main/java/com/example/ortseguros/fragments/home/cuenta/ConfiguracionFragment.kt
+++ app/src/main/java/com/example/ortseguros/fragments/home/cuenta/ConfiguracionFragment.kt
@@ -1,14 +1,21 @@
 package com.example.ortseguros.fragments.home.cuenta
 
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.Manifest
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.ortseguros.R
+import com.example.ortseguros.activities.MainActivity
 
 class ConfiguracionFragment : Fragment() {
 
@@ -17,6 +24,8 @@ class ConfiguracionFragment : Fragment() {
     lateinit var v: View
     lateinit var btnEditarCuenta : Button
     lateinit var btnPreguntasFrecuentes : Button
+    lateinit var btnAsistenciaMecanica : Button
+    lateinit var btnContacto : Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -24,6 +33,8 @@ class ConfiguracionFragment : Fragment() {
         v = inflater.inflate(R.layout.fragment_configuracion, container, false)
         btnEditarCuenta = v.findViewById(R.id.btnEditarCuenta)
         btnPreguntasFrecuentes = v.findViewById(R.id.btnPreguntasFrecuentes)
+        btnAsistenciaMecanica = v.findViewById(R.id.btnAsisMecanica)
+        btnContacto = v.findViewById(R.id.btnContacto)
         return v
 
     }
@@ -39,6 +50,35 @@ class ConfiguracionFragment : Fragment() {
         btnPreguntasFrecuentes.setOnClickListener{
             val action = ConfiguracionFragmentDirections.actionConfiguracionFragmentToPreguntasFrecuentesFragment()
             findNavController().navigate(action)
+        }
+        btnAsistenciaMecanica.setOnClickListener{
+            makePhoneCall("1122334455")
+        }
+        btnContacto.setOnClickListener {
+            makePhoneCall("1166778899")
+        }
+    }
+
+    private fun makePhoneCall(number: String) {
+
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CALL_PHONE), 1)
+        }else {
+            val dialIntent = Intent(Intent.ACTION_CALL)
+            dialIntent.data = Uri.parse("tel:$number")
+            startActivity(dialIntent)
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == 1) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            }
         }
     }
 
